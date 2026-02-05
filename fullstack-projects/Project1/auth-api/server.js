d33b7599765d53d1fa356820bce76c583974ee7d
Missantech <Missantech@Damas.local>
2026-02-05 12:16:02 +0400

    backend deploy

--- fullstack-projects/Project1/auth-api/server.js
+++ fullstack-projects/Project1/auth-api/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import noteRoutes from "./routes/noteRoutes.js";
+import path from 'path'
 
 dotenv.config();
 connectDB();
@@ -33,3 +34,14 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
+
+// Env setup: 
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static('client/dist'))
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', "dist", 'index.html'));
+  });
+}
