ddb48c7ee20c5a4cb2a0d7e8d34deb09084de62f
Missantech <Missantech@Damas.local>
2026-02-05 13:08:20 +0400

    Change the backend url in API.js file in frontend folder

--- fullstack-projects/Project1/noes-client/src/pages/api.js
+++ fullstack-projects/Project1/noes-client/src/pages/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: "https://project1-backend-dgxe.onrender.com/", // 'http://localhost:5000'
     withCredentials: true,
 })
 
