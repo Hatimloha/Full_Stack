872f325f5ffc903b57e264966e0516b512e80986
Missantech <Missantech@Damas.local>
2026-02-05 13:01:30 +0400

    app.get(/.*/, (req, res) - correct the path-to-regexp

--- fullstack-projects/Project1/auth-api/server.js
+++ fullstack-projects/Project1/auth-api/server.js
@@ -41,7 +41,7 @@ app.listen(PORT, () => {
 if(process.env.NODE_ENV === "production"){
   app.use(express.static('client/dist'))
   
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve('client', "dist", 'index.html'));
   });
 }
